08c7580927f5f7491b35dc2c741209c2a5679871
sohachimi <hachimisoufain11@gmail.com>
2025-11-15 14:47:02 +0100

    panel & point

--- Canvas/Canvas.java
+++ Canvas/Canvas.java
@@ -1,1 +1,37 @@
 package Canvas;
+
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.ArrayList;
+
+import javax.swing.JPanel;
+
+public class Canvas extends JPanel {
+    private boolean isRunning = false;
+    private final ArrayList<Point> points = new ArrayList<>();
+
+    public Canvas() {
+        this.setBackground(Color.BLACK);
+        if (!isRunning) {
+            addMouseListener(new MouseAdapter() {
+                @Override
+                public void mousePressed(MouseEvent e) {
+                    points.add(e.getPoint());
+                    repaint();
+                }
+            });
+        }
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        g.setColor(Color.WHITE);
+        for (Point p : points) {
+            g.fillOval(p.x - 4, p.y - 4, 5, 5);
+        }
+    }
+
+}
--- Main.java
+++ Main.java
@@ -1,5 +1,19 @@
+
+
+import javax.swing.JFrame;
+
+import Canvas.Canvas;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        JFrame frame = new JFrame("Jaikin Animation");
+        frame.setSize(1000, 500);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+
+        Canvas canvas = new Canvas();
+        frame.add(canvas);
+
+        frame.setVisible(true);
     }
 }
